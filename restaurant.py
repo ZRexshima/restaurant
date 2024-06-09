3fe46d8d035d59430c70fc746d15371373f59587
Michael Hensley <29671149+ZRexshima@users.noreply.github.com>
2024-06-09 11:41:03 +0900

    implements IceCreamStand, uses random.randint for testing

--- restaurant.py
+++ restaurant.py
@@ -1,3 +1,4 @@
+import random
 from string import capwords
 import time
 
@@ -21,6 +22,11 @@ class Restaurant:
         print(f"{self.restaurant_name} has served {self.number_served}")
 
 
+class IceCreamStand(Restaurant):
+    def __init__(self, restaurant_name, cuisine_type="ice cream"):
+        super().__init__(restaurant_name, cuisine_type)
+
+
 def main():
     little_shop = Restaurant('the little shop', 'burgers')
     taco_stand = Restaurant("Holy Guacamole", "tacos")
@@ -34,16 +40,24 @@ def main():
     print(f"{little_shop.restaurant_name} has served {little_shop.number_served}")
 
     while little_shop.number_served < 200:
-        serving = 8
+        serving = random.randint(0,10)
         difference = 200 - little_shop.number_served
         if difference < serving:
             little_shop.increment_number_served(difference)
             little_shop.print_number_served()
             break
         else:
-            little_shop.increment_number_served(8)
+            little_shop.increment_number_served(serving)
             little_shop.print_number_served()
             time.sleep(1)
+
+    print()
+    frosty = IceCreamStand("Frosty Kool")
+    frosty.describe_restaurant()
+    frosty.set_number_served(50)
+    frosty.print_number_served()
+    frosty.increment_number_served(5)
+    frosty.print_number_served()
 
 
 if __name__ == "__main__":
